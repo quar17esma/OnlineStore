49bcce8eece3fa5d5f9b259149d46e76178e60dc
quar17esma <quar17esma@gmail.com>
2017-09-12 14:19:44 +0300

    initial commit

---
+++ src/main/java/com/serhii/shutyi/dao/impl/JDBCDaoFactory.java
@@ -0,0 +1,30 @@
+package com.serhii.shutyi.dao.impl;
+
+
+import com.serhii.shutyi.Config;
+import com.serhii.shutyi.dao.ClientDAO;
+import com.serhii.shutyi.dao.DaoFactory;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class JDBCDaoFactory extends DaoFactory {
+
+    Connection getConnection(){
+        Config config = Config.getInstance();
+        Connection connection;
+        try {
+            connection = DriverManager
+                    .getConnection( config.getUrl() , config.getUser(),
+                            config.getPass() );
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return connection;
+    }
+    @Override
+    public ClientDAO createClientDAO() {
+        return new JDBCClientDAO(getConnection());
+    }
+}
