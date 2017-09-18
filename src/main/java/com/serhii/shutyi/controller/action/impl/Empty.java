219cb016bbafc67adecc684f0258498eb7723df9
quar17esma <quar17esma@gmail.com>
2017-09-18 15:58:49 +0300

    added Empty action

---
+++ src/main/java/com/serhii/shutyi/controller/action/impl/Empty.java
@@ -0,0 +1,17 @@
+package com.serhii.shutyi.controller.action.impl;
+
+import com.serhii.shutyi.controller.action.Action;
+import com.serhii.shutyi.controller.manager.ConfigurationManager;
+import javax.servlet.http.HttpServletRequest;
+
+
+public class Empty implements Action {
+    @Override
+    public String execute(HttpServletRequest request) {
+        String page = null;
+
+        page = ConfigurationManager.getProperty("path.page.index");
+
+        return page;
+    }
+}
