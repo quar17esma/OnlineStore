5dfc576b51adadfbfaa9e6559f194c8b4ed0437d
quar17esma <quar17esma@gmail.com>
2017-09-18 16:09:22 +0300

    added Logout action

---
+++ src/main/java/com/serhii/shutyi/controller/action/impl/Logout.java
@@ -0,0 +1,18 @@
+package com.serhii.shutyi.controller.action.impl;
+
+import com.serhii.shutyi.controller.action.Action;
+import com.serhii.shutyi.controller.manager.ConfigurationManager;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class Logout implements Action {
+
+    @Override
+    public String execute(HttpServletRequest request) {
+        String page = ConfigurationManager.getProperty("path.page.index");
+        
+        request.getSession().invalidate();
+
+        return page;
+    }
+}
