0f5f0cca8c43c7a07028b8d1cc2f93ffd3b9be11
quar17esma <quar17esma@gmail.com>
2017-09-18 16:14:20 +0300

    refactor

--- src/main/java/com/serhii/shutyi/controller/action/impl/Logout.java
+++ src/main/java/com/serhii/shutyi/controller/action/impl/Logout.java
@@ -10,7 +10,7 @@ public class Logout implements Action {
     @Override
     public String execute(HttpServletRequest request) {
         String page = ConfigurationManager.getProperty("path.page.index");
-        
+
         request.getSession().invalidate();
 
         return page;
