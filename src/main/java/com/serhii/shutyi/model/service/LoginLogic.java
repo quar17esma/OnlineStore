4df599acc1014aff0bb12a7372d9c8846e41caff
quar17esma <quar17esma@gmail.com>
2017-09-19 17:26:42 +0300

    change password

--- src/main/java/com/serhii/shutyi/model/service/LoginLogic.java
+++ src/main/java/com/serhii/shutyi/model/service/LoginLogic.java
@@ -3,7 +3,7 @@ package com.serhii.shutyi.model.service;
 
 public class LoginLogic {
     private final static String ADMIN_LOGIN = "admin";
-    private final static String ADMIN_PASS = "Qwe123";
+    private final static String ADMIN_PASS = "admin";
 
     public static boolean checkLogin(String enterLogin, String enterPass) {
         return ADMIN_LOGIN.equals(enterLogin) && ADMIN_PASS.equals(enterPass);
