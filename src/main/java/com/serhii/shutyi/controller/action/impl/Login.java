584e5d49b6dc5c55bef9ae5ba5322f1cf258490b
quar17esma <quar17esma@gmail.com>
2017-09-19 17:24:44 +0300

    get goods on main page on login action

--- src/main/java/com/serhii/shutyi/controller/action/impl/Login.java
+++ src/main/java/com/serhii/shutyi/controller/action/impl/Login.java
@@ -3,9 +3,16 @@ package com.serhii.shutyi.controller.action.impl;
 import com.serhii.shutyi.controller.action.Action;
 import com.serhii.shutyi.controller.manager.ConfigurationManager;
 import com.serhii.shutyi.controller.manager.LabelManager;
+import com.serhii.shutyi.dao.DaoFactory;
+import com.serhii.shutyi.dao.GoodDAO;
+import com.serhii.shutyi.dao.impl.JDBCGoodDAO;
+import com.serhii.shutyi.model.entity.Client;
+import com.serhii.shutyi.model.entity.Good;
 import com.serhii.shutyi.model.service.LoginLogic;
 
 import javax.servlet.http.HttpServletRequest;
+import java.sql.Connection;
+import java.util.List;
 
 public class Login implements Action {
 
@@ -18,6 +25,16 @@ public class Login implements Action {
 
             if (LoginLogic.checkLogin(login, pass)) {
                 request.setAttribute("user", login);
+
+                List<Good> goods = null;
+                DaoFactory daoFactory = DaoFactory.getInstance();
+                try(GoodDAO goodDAO = daoFactory.createGoodDAO()) {
+                    goods = goodDAO.findAll();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                request.setAttribute("goods", goods);
+
                 page = ConfigurationManager.getProperty("path.page.main");
             } else {
                 request.setAttribute("errorLoginPassMessage", LabelManager.getProperty("message.login.error"));
