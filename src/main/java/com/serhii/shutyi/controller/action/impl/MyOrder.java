2db54509bc095b28b2c00010ec37cd7dc9079429
quar17esma <quar17esma@gmail.com>
2017-09-23 21:20:57 +0300

    added empty cart message

--- src/main/java/com/serhii/shutyi/controller/action/impl/MyOrder.java
+++ src/main/java/com/serhii/shutyi/controller/action/impl/MyOrder.java
@@ -2,6 +2,7 @@ package com.serhii.shutyi.controller.action.impl;
 
 import com.serhii.shutyi.controller.action.Action;
 import com.serhii.shutyi.controller.manager.ConfigurationManager;
+import com.serhii.shutyi.controller.manager.LabelManager;
 import com.serhii.shutyi.model.entity.Order;
 
 import javax.servlet.http.HttpServletRequest;
@@ -9,15 +10,15 @@ import javax.servlet.http.HttpServletRequest;
 public class MyOrder implements Action {
     @Override
     public String execute(HttpServletRequest request) {
-        String page = ConfigurationManager.getProperty("path.page.my.order");
-
         Order order = (Order) request.getSession().getAttribute("order");
-        if (order != null) {
+        if (order != null && !order.getGoods().isEmpty()) {
             request.setAttribute("goods", order.getGoods());
         } else {
+            request.setAttribute("emptyOrderMessage",
+                    LabelManager.getProperty("message.empty.order"));
             //корзина пуста
         }
 
-        return page;
+        return ConfigurationManager.getProperty("path.page.my.order");
     }
 }
