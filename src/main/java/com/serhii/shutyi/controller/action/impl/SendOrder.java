afe7cc4234a1a8b7c146ef702e451ff3a24d4401
quar17esma <quar17esma@gmail.com>
2017-09-23 21:39:36 +0300

    send order service

--- src/main/java/com/serhii/shutyi/controller/action/impl/SendOrder.java
+++ src/main/java/com/serhii/shutyi/controller/action/impl/SendOrder.java
@@ -2,29 +2,20 @@ package com.serhii.shutyi.controller.action.impl;
 
 import com.serhii.shutyi.controller.action.Action;
 import com.serhii.shutyi.controller.manager.ConfigurationManager;
-import com.serhii.shutyi.dao.DaoFactory;
-import com.serhii.shutyi.dao.OrderDAO;
 import com.serhii.shutyi.model.entity.Order;
+import com.serhii.shutyi.model.service.SendOrderService;
 
 import javax.servlet.http.HttpServletRequest;
-import java.time.LocalDateTime;
 
 public class SendOrder implements Action {
     @Override
     public String execute(HttpServletRequest request) {
-        String page = ConfigurationManager.getProperty("path.page.main");
-
         Order order = (Order) request.getSession().getAttribute("order");
 
         if (order != null) {
-            try (OrderDAO orderDAO = DaoFactory.getInstance().createOrderDAO()) {
-                order.setOrderedAt(LocalDateTime.now());
-                orderDAO.insert(order);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+            SendOrderService.getInstance().sendOrder(order);
         }
 
-        return page;
+        return ConfigurationManager.getProperty("path.page.main");
     }
 }
---
+++ src/main/java/com/serhii/shutyi/model/service/SendOrderService.java
@@ -0,0 +1,28 @@
+package com.serhii.shutyi.model.service;
+
+import com.serhii.shutyi.dao.DaoFactory;
+import com.serhii.shutyi.dao.OrderDAO;
+import com.serhii.shutyi.model.entity.Order;
+
+import java.time.LocalDateTime;
+
+public class SendOrderService {
+    DaoFactory factory = DaoFactory.getInstance();
+
+    private static class Holder {
+        private static SendOrderService INSTANCE = new SendOrderService();
+    }
+
+    public static SendOrderService getInstance() {
+        return SendOrderService.Holder.INSTANCE;
+    }
+
+    public void sendOrder(Order order) {
+        try (OrderDAO orderDAO = factory.createOrderDAO()) {
+            order.setOrderedAt(LocalDateTime.now());
+            orderDAO.insert(order);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
