c809616485786adec230a1df3a4f09147ec08031
quar17esma <quar17esma@gmail.com>
2017-09-23 20:55:22 +0300

    buyNowService

--- src/main/java/com/serhii/shutyi/controller/action/impl/BuyNow.java
+++ src/main/java/com/serhii/shutyi/controller/action/impl/BuyNow.java
@@ -2,27 +2,20 @@ package com.serhii.shutyi.controller.action.impl;
 
 import com.serhii.shutyi.controller.action.Action;
 import com.serhii.shutyi.controller.manager.ConfigurationManager;
-import com.serhii.shutyi.dao.DaoFactory;
-import com.serhii.shutyi.dao.GoodDAO;
 import com.serhii.shutyi.model.entity.Good;
+import com.serhii.shutyi.model.service.BuyNowService;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.Optional;
 
 public class BuyNow implements Action {
     @Override
     public String execute(HttpServletRequest request) {
-        String page = ConfigurationManager.getProperty("path.page.buy.now");
-
         int goodId = Integer.parseInt(request.getParameter("goodId"));
 
-        try(GoodDAO goodDAO = DaoFactory.getInstance().createGoodDAO()) {
-            Optional<Good> good = goodDAO.findById(goodId);
-            request.setAttribute("good", good.get());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        Good good = BuyNowService.getInstance().getGoodById(goodId);
 
-        return page;
+        request.setAttribute("good", good);
+
+        return ConfigurationManager.getProperty("path.page.buy.now");
     }
 }
---
+++ src/main/java/com/serhii/shutyi/model/service/BuyNowService.java
@@ -0,0 +1,27 @@
+package com.serhii.shutyi.model.service;
+
+import com.serhii.shutyi.dao.DaoFactory;
+import com.serhii.shutyi.dao.GoodDAO;
+import com.serhii.shutyi.model.entity.Good;
+
+public class BuyNowService {
+    DaoFactory factory = DaoFactory.getInstance();
+
+    private static class Holder {
+        private static BuyNowService INSTANCE = new BuyNowService();
+    }
+
+    public static BuyNowService getInstance() {
+        return BuyNowService.Holder.INSTANCE;
+    }
+
+    public Good getGoodById(int goodId) {
+        Good good = null;
+        try(GoodDAO goodDAO = factory.createGoodDAO()) {
+            good = goodDAO.findById(goodId).get();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return good;
+    }
+}
