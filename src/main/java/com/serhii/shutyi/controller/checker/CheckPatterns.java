4f3a8395f6e568fb3163186f8256f04d6f2d768a
quar17esma <quar17esma@gmail.com>
2017-10-03 15:41:20 +0300

    delete unused patterns

--- src/main/java/com/serhii/shutyi/controller/checker/CheckPatterns.java
+++ src/main/java/com/serhii/shutyi/controller/checker/CheckPatterns.java
@@ -11,9 +11,4 @@ import java.util.regex.Pattern;
 public interface CheckPatterns {
     Pattern NAME = Pattern.compile("[A-Z][a-z]+");
     Pattern EMAIL = Pattern.compile("[a-zA-Z1-9\\-\\._]+@[a-z1-9]+(.[a-z1-9]+){1,}");
-    Pattern NICKNAME = Pattern.compile("[a-zA-Z0-9_.-]{3,}");
-    Pattern ANY = Pattern.compile(".*");
-    Pattern PHONE_NUMBER = Pattern.compile("\\+?\\d+([\\(\\s\\-]?\\d+[\\)\\s\\-]?[\\d\\s\\-]+)?");
-    Pattern POST_INDEX = Pattern.compile("[0-9]{5}");
-    Pattern NUMBER = Pattern.compile("[0-9]{1,5}[a-z]?");
 }
