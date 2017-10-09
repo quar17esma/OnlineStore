74c6bd5d6e6e3e5e3af101cbb23c9c9301518408
quar17esma <quar17esma@gmail.com>
2017-10-09 14:38:07 +0300

    deleted comments

--- src/main/java/com/serhii/shutyi/controller/checker/CheckPatterns.java
+++ src/main/java/com/serhii/shutyi/controller/checker/CheckPatterns.java
@@ -2,12 +2,6 @@ package com.serhii.shutyi.controller.checker;
 
 import java.util.regex.Pattern;
 
-/**
- * The {@code CheckPatterns} interface contains patterns to check user's input.
- *
- * @author S.Shutyi
- * Created on 25.09.2017.
- */
 public interface CheckPatterns {
     Pattern NAME = Pattern.compile("[A-Z][A-Za-z ]+");
     Pattern EMAIL = Pattern.compile("[a-zA-Z1-9\\-._]+@[a-z1-9]+(.[a-z1-9]+)+");
